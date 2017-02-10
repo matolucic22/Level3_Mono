87689796cf2ee883b377668ba21ac0b5e4d54ef8
Mato Lučić <mlucic@etfos.hr>
2017-02-10 13:21:44 +0100

    Kreiran projekt

---
+++ DAL/DAL/Models/VehicleMake.cs
@@ -0,0 +1,18 @@
+﻿using DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class VehicleMake
+    {
+        public Guid Id { get; set; }
+        public String Name { get; set; }
+        public String Abrv { get; set; }
+
+        public virtual ICollection<VehicleModel> VehicleModels { get; set; }//1 make ima vise modela
+    }
+}
---
+++ DAL/DAL/Models/VehicleModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Models
+{
+    class VehicleModel
+    {
+        public Guid Id { get; set; }
+        public Guid VehicleMakeId { get; set; }
+        public String Name { get; set; }
+        public String Abrv { get; set; }
+
+        public virtual VehicleMake VehicleMake { get; set; }//poziva jedan make di je npr id=1; - 1 make ima vise modela. Zato što svaki model ima 1 make m
+    }
+}
