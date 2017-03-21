215b8802cebb02653fe03cb35372bd9962f2376f
Mato Lučić <mlucic@etfos.hr>
2017-03-21 15:19:54 +0100

    Napravljen ispis iz tablice za Make

---
+++ Project.MVC.WebAPI/app/Components/VehicleMake/Controllers/AddVehicleMakeController.js
@@ -0,0 +1,17 @@
+﻿/*routerApp.controller("AddVehicleMakeController", function ($scope, $http, $location, $window) {
+    $scope.addVehicleMake=function()
+    {
+        var vehicle =
+            {
+                Name: $scope.Name,
+                Abrv: $scope.Abrv
+            };
+        $http.post('/api/VehicleMake/Add', vehicle).then(function (data) {
+
+            $scope.response = data;
+            $window.alert("Succesfully added!");
+            $location.path('/vehicleMake');
+
+        });
+    }
+});*/
---
+++ Project.MVC.WebAPI/app/app.Module.js
@@ -0,0 +1,24 @@
+﻿var routerApp = angular.module('routerApp', ['ui.router', 'angularUtils.directives.dirPagination']);//kolekcije kontrolera, modela, servisa.
+
+routerApp.config(function($stateProvider, $urlRouterProvider)//.otherwise-default route
+{
+    $urlRouterProvider.otherwise('/VehicleMake');
+
+    $stateProvider.state('VehicleMake', {//ui-sref=VhicleMake-->the name of state that we want to refer
+        url: '/VehicleMake',//URL that we want to navigate to--when click on VehicleMake we activate this
+        controller: 'VehicleMakeController',//controller iz kojeg uzima?
+        views: {
+            "root": {
+                templateUrl: 'app/Components/VehicleMake/Views/IndexVehMake.html'
+            }
+        }
+    }).state('VehicleMakeAdd', {
+        url: '/addVehicleMake',
+        views: {
+            "root": {
+                
+            }
+        }
+    })
+
+    ;})
